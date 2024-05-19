de4d07bff125ed65a60645e711f4746d3d88e872
James <89285665+ji8sw@users.noreply.github.com>
2024-05-19 21:03:32 +1000

    Some small changes

--- main.py
+++ main.py
@@ -4,23 +4,21 @@ token = 'c7d8b6fbdc8dca'
 print("WARNING: LOCATIONS ARE RETRIVED FROM THE ISP, MEANING THAT YOU CANNOT GET THE ADDRESS, THIS ALSO MEANS THAT THE PERSON DOES NOT LIVE WHERE THE IP IS LOCATED")
 ip = input("IP ADDRESS:\n")
 print("Advanced mode or Simple mode?")
-mode = input("Advanced for Advanced or Simple for Simple\n")
+mode = input("Advanced for Advanced or Simple for Simple\n").lower()
 if mode == "simple":
     handler = ipinfo.getHandler(token)
     details = handler.getDetails(ip)
     substring1 = "domain"
-    print(details.country_name)
-    print(details.postal)
-    print(details.region)
-    print(details.city)
+    print("Country: ", details.country_name)
+    print("Post Code: ", details.postal)
+    print("Region: ", details.region)
+    print("City: ", details.city)
 
     if 'domain' in locals():
-        print(details.domain)
-    else:
-        print("No domain found, possibly innacurate")
-    print(details.loc)
+        print("Domain: ", details.domain)
+    print("Coordinates: ", details.loc)
     if 'abuse' in locals():
-        print(details.abuse)
+        print("Personal Information: ", details.abuse)
     else:
          print("No personal information found, this is common")
 elif mode == "advanced":
